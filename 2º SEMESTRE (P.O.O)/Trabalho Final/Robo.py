c985cf946dfbb9115daf0437906210d4252d146e
GaabrielOliveira <gabfelipe2010@gmail.com>
2024-09-17 17:20:00 -0400

    alteranto a classe Robo

--- 2º SEMESTRE (P.O.O)/Trabalho Final/Robo.py
+++ 2º SEMESTRE (P.O.O)/Trabalho Final/Robo.py
@@ -26,17 +26,3 @@ class Robo:
     def precisa_de_medico(self):
         return self.vida < Robo.nivel_critico
 
-
-class RoboMedico(Robo):
-    def __init__(self, nome: str):
-        super().__init__(nome)
-        self.poder_de_cura = random.uniform(0, 1)
-
-    def curar(self, robo):
-        if robo.vida < 1 and self.vida >= robo.vida:
-            cura = min(1 - robo.vida, self.poder_de_cura)
-            robo.vida += round(cura, 2)
-            print(f'{self.nome} curou {robo.nome}. Nova vida: {robo.vida}')
-
-    def atender_chamado(self):
-        return random.choice([True, False])
